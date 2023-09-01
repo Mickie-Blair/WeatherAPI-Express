3a047d172aecc9fb421c230ffb1d7bf1bd7fe089
Mickie Blair <contact@mickieblair.com>
2023-09-01 12:08:29 -0600

    add dotenv and update routes for API call

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ import axios from "axios";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-
+import 'dotenv/config';
 import path from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
@@ -31,6 +34,35 @@ app.use(
 app.get('/', (req, res) => {
     let data = {};
     res.render('templates/index.ejs', data);
+});
+
+app.post('/get-weather', async (req, res) => {
+    
+    let latitude = req.body['latitude'];
+    let longitude = req.body['longitude'];
+    console.log('get weather', latitude, longitude);
+
+    console.log(req.body)
+
+
+    try {
+        let api_url = "https://api.openweathermap.org/data/2.5/weather";
+        const result = await axios.get(api_url, {
+          params: {
+            lat: latitude,
+            lon: longitude,
+            units: 'imperial',
+            appid: process.env.OPEN_WEATHER_API_KEY,
+          },
+        });
+        res.json(result.data);
+      } catch (error) {
+        let error_obj = {
+            error: error.message,
+        }
+        res.json({ content: error_obj });
+      }
+
 });
 
 app.listen(port, ()=>{
